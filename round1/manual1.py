cbe073ed885db70d4273e87ff1cd5491e7a884e2
Timothy Kostolansky <39891386+tim0120@users.noreply.github.com>
2024-04-10 00:11:11 -0400

    manual1 success

--- round1/manual1.py
+++ round1/manual1.py
@@ -1,24 +1,29 @@
 import numpy as np
+from tqdm import tqdm
 
-num_reserve_samples = 1000
+num_reserve_samples = 10000
 min_bid, max_bid = 900, 1000
-reserve_sample = np.random.triangular(min_bid, max_bid, max_bid, num_reserve_samples)
 
-best_return = 0
-best_range = None
-for min_bid_i in range(min_bid, max_bid+1, 1):
-    for max_bid_i in range(min_bid_i, max_bid+1, 1):
-        will_buy = reserve_sample > max_bid_i
-        buying_reserves = reserve_sample[will_buy]
-        buy_i = np.mean(np.where(
-            min_bid_i > buying_reserves,
-            min_bid_i,
-            max_bid_i
-        ))
-        avg_return = max_bid - buy_i
-        if avg_return > best_return:
-            best_return = avg_return
-            best_range = (min_bid_i, max_bid_i)
+epochs = 250
+best_ranges = []
+for _ in tqdm(range(epochs)):
+    reserve_sample = np.random.triangular(min_bid, max_bid, max_bid, num_reserve_samples)
+    best_return = 0
+    best_range = None
+    for min_bid_i in range(min_bid, max_bid, 1):
+        for max_bid_i in range(min_bid_i, max_bid+1, 1):
+            buying_reserves = reserve_sample[reserve_sample < max_bid_i]
+            buy_i = np.sum(np.where(
+                min_bid_i > buying_reserves,
+                min_bid_i,
+                max_bid_i
+            ))
+            return_i = max_bid * len(buying_reserves) - buy_i
+            if return_i > best_return:
+                best_return = return_i
+                best_range = [min_bid_i, max_bid_i]
+    best_ranges.append(best_range)
 
-print(best_return)
-print(best_range)
+# print(best_ranges)
+best_best_range = np.mean(np.array(best_ranges), axis=0)
+print(best_best_range)
