1c0f656a876b79be33c6090e7da1120d29be68d3
Steven Henry <spmh@mit.edu>
2024-04-11 01:50:49 -0400

    added csv of all starfruit trades

--- round1/finding_trends_in_data.py
+++ round1/finding_trends_in_data.py
@@ -1,29 +1,27 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-
 
 # importing all data
-day0_trades = pd.read_csv('/Users/stevenhenry/Documents/imc_prosperity/round1/trades_round_1_day_0_nn.csv', delimiter=';')
-day1_trades = pd.read_csv('/Users/stevenhenry/Documents/imc_prosperity/round1/trades_round_1_day_-1_nn.csv', delimiter=';')
-day2_trades = pd.read_csv('/Users/stevenhenry/Documents/imc_prosperity/round1/trades_round_1_day_-2_nn.csv', delimiter=';')
+df_prev2 = pd.read_csv('/Users/stevenhenry/Documents/imc_prosperity/round1/trades_round_1_day_-2_nn.csv', delimiter=';')
+df_prev1 = pd.read_csv('/Users/stevenhenry/Documents/imc_prosperity/round1/trades_round_1_day_-1_nn.csv', delimiter=';')
+df_prev0 = pd.read_csv('/Users/stevenhenry/Documents/imc_prosperity/round1/trades_round_1_day_0_nn.csv', delimiter=';')
+last_timestamp_day0 = df_prev2['timestamp'].iloc[-1]  
+last_timestamp_day1 = df_prev1['timestamp'].iloc[-1] + last_timestamp_day0 + 1 
+df_prev1['timestamp'] += last_timestamp_day0 + 1
+df_prev0['timestamp'] += last_timestamp_day1 + 1
+all_trades = pd.concat([df_prev2, df_prev1, df_prev0], ignore_index=True)
+df_starfruit = all_trades[all_trades['symbol']=='STARFRUIT']
+df_starfruit = df_starfruit.groupby('timestamp').last().reset_index()
+df_starfruit.to_csv('starfruit_all_trades.csv',index=False)
 
 # splitting up the data into amethyst and starfruit
 # dictionaries will take the form --> [(timestamp, price, quantity)]
-amethyst = [] 
-starfruit = []
+# amethyst = [] 
+# starfruit = []
 
-for index, row in day0_trades.iterrows():
-    timestamp = row['timestamp']
-    price = row['price']
-    symbol = row['symbol']
-    quantity = row['quantity']
-    if symbol == 'STARFRUIT':
-        starfruit.append((timestamp, price, quantity))
-    else:
-        amethyst.append((timestamp, price, quantity))
-
-# plotting graphs
-# Assuming 'amethyst' and 'starfruit' are your lists of tuples with (timestamp, price, quantity)
-amethyst_df = pd.DataFrame(amethyst, columns=['timestamp', 'price', 'quantity'])
-starfruit_df = pd.DataFrame(starfruit, columns=['timestamp', 'price', 'quantity'])
-
+# for index, row in all_days_trades.iterrows():
+#     timestamp = row['timestamp']
+#     price = row['price']
+#     symbol = row['symbol']
+#     quantity = row['quantity']
+#     if symbol == 'STARFRUIT':
+#         starfruit.append((timestamp, price, quantity))
